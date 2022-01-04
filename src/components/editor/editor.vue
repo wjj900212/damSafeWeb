9aa601057c436b2ac45cd2d61840af34b8a294b1
xieyaode <xyd08088>
2022-01-04 15:48:05 +0800

    富文本组件设置

---
+++ src/components/editor/editor.vue
@@ -0,0 +1,222 @@
+<template>
+  <div class="tinymce-box">
+    <editor-te v-model="content" :init="init" :disabled="disabled" :key="randomKey"></editor-te>
+  </div>
+</template>
+
+<script>
+  // 引入tinymce编辑器
+  import editorTe from '@tinymce/tinymce-vue'
+
+  // 引入方式引入node_modules里的tinymce相关文件文件
+  import tinymce from 'tinymce/tinymce' // tinymce默认hidden，不引入则不显示编辑器
+  import 'tinymce/themes/silver' // 编辑器主题，不引入则报错
+  import 'tinymce/icons/default' // 引入编辑器图标icon，不引入则不显示对应图标
+
+  // 引入编辑器插件
+  import 'tinymce/plugins/advlist' // 高级列表 有序 / 无序列表
+  import 'tinymce/plugins/anchor' // 锚点
+  import 'tinymce/plugins/autolink' // 自动链接
+  import 'tinymce/plugins/autoresize' // 编辑器高度自适应,注：plugins里引入此插件时，Init里设置的height将失效
+  import 'tinymce/plugins/autosave' // 自动存稿
+  // import "tinymce/plugins/charmap"; //特殊字符
+  // import "tinymce/plugins/code"; //编辑源码
+  // import "tinymce/plugins/codesample"; //代码示例
+  import 'tinymce/plugins/directionality' // 文字方向
+  // import "tinymce/plugins/emoticons"; //表情
+  // import "tinymce/plugins/fullpage"; //文档属性
+  // import "tinymce/plugins/fullscreen"; //全屏
+  import 'tinymce/plugins/help' // 帮助
+  import 'tinymce/plugins/hr' // 水平分割线
+  import 'tinymce/plugins/image' // 插入编辑图片
+  import 'tinymce/plugins/importcss' // 引入css
+  import 'tinymce/plugins/insertdatetime' // 插入日期时间
+  import 'tinymce/plugins/link' // 超链接
+  import 'tinymce/plugins/lists' // 列表插件
+  import 'tinymce/plugins/media' // 插入编辑媒体
+  // import "tinymce/plugins/nonbreaking"; //插入不间断空格
+  import 'tinymce/plugins/pagebreak' // 插入分页符
+  // import "tinymce/plugins/paste"; //粘贴插件
+  import 'tinymce/plugins/preview' // 预览
+  // import "tinymce/plugins/print"; //打印
+  // import "tinymce/plugins/quickbars"; //快速工具栏
+  // import "tinymce/plugins/save"; //保存
+  import 'tinymce/plugins/searchreplace' // 查找替换
+  // import 'tinymce/plugins/spellchecker'  //拼写检查，暂未加入汉化，不建议使用
+  // import "tinymce/plugins/tabfocus"; //切入切出，按tab键切出编辑器，切入页面其他输入框中
+  import 'tinymce/plugins/table' // 表格
+  // import "tinymce/plugins/template"; //内容模板
+  import 'tinymce/plugins/textcolor' // 文字颜色
+  // import "tinymce/plugins/textpattern"; //快速排版
+  // import "tinymce/plugins/toc"; //目录生成器
+  // import "tinymce/plugins/visualblocks"; //显示元素范围
+  import 'tinymce/plugins/visualchars' // 显示不可见字符
+  import 'tinymce/plugins/wordcount' // 字数统计
+
+  export default {
+    name: 'tinymceEditor',
+    components: {
+      editorTe
+    },
+    props: {
+      disabled: {
+        type: Boolean,
+        default: false
+      },
+      putParams: {
+        type: Object,
+        default: function () {
+          return {}
+        }
+      },
+      putUrl: {
+        type: String,
+        default: ''
+      },
+      delUrl: {
+        type: String,
+        default: ''
+      }
+    },
+    data() {
+      let _this = this
+      return {
+        randomKey: new Date().getTime(),
+        init: {
+          language_url: 'static/tinymce/zh_CN.js', // 引入语言包文件
+          language: 'zh_CN', // 语言类型
+          content_style: 'p {margin: 0px; border:0px ; padding: 0px;}', // 设置行间距
+          mode: 'textareas',
+          skin_url: 'static/tinymce/skins/ui/oxide', // 皮肤：浅色
+          readonly: 1,
+          // 避免图片地址和链接地址转换成相对路径
+          relative_urls: false, 
+          remove_script_host: false,
+          convert_urls: false,
+          // object_resizing:false, //禁止图片调整大小
+          plugins: 'searchreplace image media table hr pagebreak anchor insertdatetime advlist lists wordcount autosave', // 插件配置
+          // 工具栏1
+          toolbar1: 'forecolor backcolor bold italic underline strikethrough link anchor | alignleft aligncenter alignright alignjustify outdent indent lineheight',
+          // // 工具栏2 去掉的 print 打印  formatpainter axupimgs 等等
+          toolbar2: 'bullist numlist | blockquote subscript superscript | table image hr pagebreak insertdatetime | searchreplace | formatselect fontselect fontsizeselect',
+          menubar: false, // 菜单栏配置,'file edit'，设为false则隐藏，不配置则默认显示全部菜单，也可自定义配置--查看 http://tinymce.ax-z.cn/configure/editor-appearance.php --搜索“自定义菜单”
+
+          fontsize_formats: '12px 14px 16px 18px 20px 22px 24px 28px 32px 36px 48px 56px 72px', // 字体大小
+          font_formats: '微软雅黑=Microsoft YaHei,Helvetica Neue,PingFang SC;苹果苹方=PingFang SC,Microsoft YaHei;宋体=simsun,serif;仿宋体=FangSong,serif;黑体=SimHei;Arial=arial,helvetica;Arial Black=arial black,avant garde;Book Antiqua=book antiqua,palatino;',
+          height: 400, // 注：引入autoresize插件时，此属性失效
+          min_height: 300,
+          placeholder: '在这里输入文字',
+          branding: false, // tiny技术支持信息是否显示
+          resize: false, // 编辑器宽高是否可变，false-否,true-高可变，'both'-宽高均可，注意引号
+          statusbar: false, // 最下方的元素路径和字数统计那一栏是否显示
+          elementpath: false, // 元素路径是否显示
+          automatic_uploads: true, //启用或者禁止上传由 data URL 或者 blob URls表示的图像。例如，将图像从桌面拖放到编辑器上之后，生成图像。
+          toolbar_sticky: true,
+          autosave_ask_before_unload: false, // 阻止有内容时浏览器阻塞行为, 默认 true  需引入插件autosave
+          content_style: 'img {max-width:100%;}', // 直接自定义可编辑区域的css样式
+          // content_css: '/tinycontent.css',  //以css文件方式自定义可编辑区域的css样式，css文件需自己创建并引入
+
+          // images_upload_url: '/demo/upimg.php',  //后端处理程序的url
+          // images_upload_base_path: '/demo',  //相对基本路径--关于图片上传建议查看--http://tinymce.ax-z.cn/general/upload-images.php
+          // 此处为图片上传处理函数，这个直接用了base64的图片形式上传图片，
+          // 如需ajax上传可参考https://www.tiny.cloud/docs/configure/file-image-upload/#images_upload_handler
+          images_upload_handler: (blobInfo, success, failure) => {
+            //   const img = "data:image/jpeg;base64," + blobInfo.base64();
+            //   console.log(blobInfo, blobInfo.blob());
+            let fd = new FormData()
+            console.log(_this.putParams)
+            if (JSON.stringify(_this.putParams) != "{}") {
+              Object.keys(_this.putParams).forEach(v => {
+                fd.append(v, _this.putParams[v])
+              })
+            }
+            fd.append('file', blobInfo.blob())
+            _this.$upload(_this.putUrl, fd).then((res) => {
+              if (res.data.code !== 1) {
+                failure('HTTP Error:' + res.data.msg)
+                return
+              }
+              let rr = res.data.data
+              success(rr)
+            })
+          },
+          // file_picker_callback: function (callback, value, meta) {
+          //   if (meta.filetype === "media") {
+          //     // 模拟上传本地视频
+          //     let input = document.createElement("input");
+          //     input.setAttribute("type", "file");
+          //     input.setAttribute("accept", ".mp4");
+          //     input.onchange = function () {
+          //       let file = this.files[0];
+          //       // console.log(file);
+          //       let fd = new FormData();
+          //       fd.append("video", file);
+          //       // _this.$upload("...", fd).then((res) => {
+          //       //   let rr = res.data.data;
+          //       //   callback(rr);
+          //       // });
+          //     };
+          //     input.click();
+          //   }
+          // },
+          setup: function (ed) {
+            ed.on('KeyDown', function (e) {
+              if ((e.keyCode == 8 || e.keyCode == 46) && ed.selection) { // delete& backspace keys
+                var selectedNode = ed.selection.getEnd()
+                // console.log(selectedNode, selectedNode.nodeName)
+                if (selectedNode && selectedNode.nodeName == 'IMG') {
+                  // console.log(selectedNode.src)
+                  let ss = selectedNode.src
+                  // ss.indexOf('isafety.zgcsafety.com.cn') != -1 && 
+                  if (getUrlParam(ss, 'filename')) {
+                    // 确认为上传的图片且存在名称
+                    _this.delImg.push(getUrlParam(ss, 'filename'))
+                  }
+                }
+              }
+            })
+          }
+        },
+        content: '',
+        delImg: []
+      }
+    },
+    created() {
+      this.delImg = []
+    },
+    mounted() {
+      tinymce.init({})
+      this.randomKey = new Date().getTime()
+    },
+    methods: {
+      delPic() {
+        // console.log(this.delImg)
+        let obj = {}
+        if (JSON.stringify(this.putParams) != "{}") {
+          Object.keys(this.putParams).forEach(k => {
+            obj[k] = this.putParams[k]
+          })
+        }
+        obj.list = this.delImg
+        // console.log(obj)
+        this.$postDate(this.delUrl, obj).then(res => {
+          this.delImg = []
+        })
+      }
+    },
+    deactivated() {
+      if (tinymce) {
+        tinymce.destroy()
+      }
+    },
+  }
+
+  // 获取url中的参数
+  function getUrlParam(url, name) {
+    var reg = new RegExp('(^|&)' + name + '=([^&]*)(&|$)') // 构造一个含有目标参数的正则表达式对象
+    var r = url.substr(1).match(reg) // 匹配目标参数
+    if (r != null) return decodeURIComponent(r[2])
+    return null // 返回参数值
+  }
+
+</script>
--- src/views/system/reservoir/reservoir.vue
+++ src/views/system/reservoir/reservoir.vue
@@ -1,301 +1,220 @@
 <template>
   <a-card :bordered="false" class="card-area">
-    <div :class="advanced ? 'search' : null">
+    <div>
       <!-- 搜索区域 -->
       <a-form layout="horizontal">
-        <a-row >
-          <div :class="advanced ? null: 'fold'">
-            <a-col :md="12" :sm="24" >
-              <a-form-item
-                label="水库名称"
-                :labelCol="{span: 4}"
-                :wrapperCol="{span: 18, offset: 2}">
-                <a-input v-model="queryParams.username" placeholder="请输入"/>
+        <a-row>
+          <div class="fold">
+            <a-col :md="12" :sm="24">
+              <a-form-item label="水库名称" :labelCol="{span: 4}" :wrapperCol="{span: 18, offset: 2}">
+                <a-input v-model="queryParams.name" placeholder="请输入" />
               </a-form-item>
             </a-col>
-            <a-col :md="12" :sm="24" >
-              <a-form-item
-                label="所在地区"
-                :labelCol="{span: 4}"
-                :wrapperCol="{span: 18, offset: 2}">
-                <a-input v-model="queryParams.shuiku" placeholder="请输入"/>
+            <a-col :md="12" :sm="24">
+              <a-form-item label="所在地区" :labelCol="{span: 4}" :wrapperCol="{span: 18, offset: 2}">
+                <a-input v-model="queryParams.area" placeholder="请输入" />
               </a-form-item>
             </a-col>
           </div>
           <span style="float: right; margin-top: 3px;">
-            <a-button type="primary" @click="search">查询</a-button>
-            <a-button style="margin-left: 8px" @click="reset">重置</a-button>
+            <a-button type="primary">查询</a-button>
+            <a-button style="margin-left: 8px">重置</a-button>
           </span>
         </a-row>
       </a-form>
     </div>
     <div>
       <div class="operator">
-        <a-button type="primary" @click="add">添加</a-button>
+        <a-button type="primary" @click="goAdd">添加</a-button>
       </div>
       <!-- 表格区域 -->
-      <a-table ref="TableInfo"
-               :rowKey="(record,index)=>{return index}"
-               :columns="columns"
-               :dataSource="dataSource"
-               :pagination="pagination"
-               :loading="loading"
-               :scroll="{ x: 900 }"
-               @change="handleTableChange">
+      <a-table ref="TableInfo" :rowKey="(record,index)=>{return index}" :columns="columns" :dataSource="dataSource"
+        :pagination="pagination" :loading="loading" :scroll="{ x: 900 }">
+        <template slot="operation" slot-scope="text, record">
+          <div class="icons-list">
+            <a-icon type="warning" theme="twoTone" twoToneColor="#4a9ff5" title="监测场景" @click="$router.push('/system/reservoir/reservoir/scene')"></a-icon>
+            <a-icon type="info-circle" theme="twoTone" twoToneColor="#4a9ff5" title="监测点"></a-icon>
+            <a-icon type="user" title="用户"></a-icon>
+            <a-icon type="setting" theme="twoTone" twoToneColor="#4a9ff5" title="编辑"></a-icon>
+            <a-icon type="delete" theme="twoTone" twoToneColor="#4a9ff5" @click="userDelete(record)" title="删除">
+            </a-icon>
+          </div>
+        </template>
       </a-table>
     </div>
   </a-card>
 </template>
 
 <script>
-import { mapState } from 'vuex'
-export default {
-  name: 'reservoir',
-  components: {},
-  data () {
-    return {
-      advanced: false,
-      userInfo: {
-        visiable: false,
-        data: {}
-      },
-      userAdd: {
-        visiable: false
-      },
-      userEdit: {
-        visiable: false
-      },
-      queryParams: {},
-      filteredInfo: null,
-      sortedInfo: null,
-      paginationInfo: null,
-      dataSource: [],
-      selectedRowKeys: [],
-      loading: false,
-      pagination: {
-        pageSizeOptions: ['10', '20', '30', '40', '100'],
-        defaultCurrent: 1,
-        defaultPageSize: 10,
-        showQuickJumper: true,
-        showSizeChanger: true,
-        showTotal: (total, range) => `显示 ${range[0]} ~ ${range[1]} 条记录，共 ${total} 条记录`
-      }
-    }
-  },
-  computed: {
-    ...mapState({
-      currentUser: state => state.account.user
-    }),
-    columns () {
-      let { filteredInfo } = this
-      filteredInfo = filteredInfo || {}
-      return [{
-        title: '序号',
-        customRender: (text, record, index) => `${index + 1}`
-      }, {
-        title: '水库名称',
-        dataIndex: 'username'
-      }, {
-        title: '地址',
-        dataIndex: 'roleName3'
-      }, {
-        title: '管理单位',
-        dataIndex: 'roleName2'
-      }, {
-        title: '安全状态',
-        dataIndex: 'roleId',
-        customRender: (text) => {
-          switch (text) {
-            case '0':
-              return '无风险'
-            case '1':
-              return '有风险'
-          }
+  import {
+    mapState
+  } from 'vuex'
+  export default {
+    name: 'reservoir',
+    components: {},
+    data() {
+      return {
+        queryParams: {
+          name: '',
+          area: ''
         },
-        filters: [
-          {
-            text: '无风险',
-            value: '0'
+        loading: false,
+        dataSource: [{name: '121',roleId:1}],
+        pagination: {
+          pageSizeOptions: ['10', '20', '30', '40', '100'],
+          defaultCurrent: 1,
+          defaultPageSize: 10,
+          showQuickJumper: true,
+          showSizeChanger: true,
+          showTotal: (total, range) => `显示 ${range[0]} ~ ${range[1]} 条记录，共 ${total} 条记录`
+        }
+      }
+    },
+    computed: {
+      ...mapState({
+        currentUser: state => state.account.user
+      }),
+      columns() {
+        let {
+          filteredInfo
+        } = this
+        filteredInfo = filteredInfo || {}
+        return [{
+            title: '序号',
+            customRender: (text, record, index) => `${index + 1}`
+          }, {
+            title: '水库名称',
+            dataIndex: 'name'
+          }, {
+            title: '地址',
+            dataIndex: 'roleName3'
+          }, {
+            title: '管理单位',
+            dataIndex: 'roleName2'
+          }, {
+            title: '安全状态',
+            dataIndex: 'roleId',
+            customRender: (text) => {
+              switch (text) {
+                case '0':
+                  return '无风险'
+                case '1':
+                  return '有风险'
+              }
+            },
+            filters: [{
+                text: '无风险',
+                value: '0'
+              },
+              {
+                text: '有风险',
+                value: '1'
+              }
+            ],
+            filterMultiple: true,
+            filteredValue: filteredInfo.roleId || null,
+            onFilter: (value, record) => record.roleId.includes(value)
+          }, {
+            title: '监测场景',
+            dataIndex: 'roleName1'
           },
           {
-            text: '有风险',
-            value: '1'
-          }],
-        filterMultiple: true,
-        filteredValue: filteredInfo.roleId || null,
-        onFilter: (value, record) => record.roleId.includes(value)
-      }, {
-        title: '监测场景',
-        dataIndex: 'roleName1'
-      },
-      {
-        title: '水库规模',
-        dataIndex: 'userProjCount',
-        customRender: (text) => {
-          switch (text) {
-            case '0':
-              return '大(1)型'
-            case '1':
-              return '大(2)型'
-            case '2':
-              return '中型'
-            case '3':
-              return '小(1)型'
-            case '4':
-              return '小(2)型'
+            title: '水库规模',
+            dataIndex: 'userProjCount',
+            customRender: (text) => {
+              switch (text) {
+                case '0':
+                  return '大(1)型'
+                case '1':
+                  return '大(2)型'
+                case '2':
+                  return '中型'
+                case '3':
+                  return '小(1)型'
+                case '4':
+                  return '小(2)型'
+              }
+            },
+            filters: [{
+                text: '大(1)型',
+                value: '0'
+              },
+              {
+                text: '大(2)型',
+                value: '1'
+              },
+              {
+                text: '中型',
+                value: '2'
+              },
+              {
+                text: '小(1)型',
+                value: '3'
+              },
+              {
+                text: '小(2)型',
+                value: '4'
+              }
+            ],
+            filterMultiple: true,
+            filteredValue: filteredInfo.userProjCount || null,
+            onFilter: (value, record) => record.userProjCount.includes(value)
+          }, {
+            title: '水库类型',
+            dataIndex: 'userFollowCount'
+          },
+          {
+            title: '创建时间',
+            dataIndex: 'lastLoginTime'
+          }, {
+            title: '操作',
+            dataIndex: 'operation',
+            scopedSlots: {
+              customRender: 'operation'
+            }
           }
-        },
-        filters: [
-          {text: '大(1)型', value: '0'},
-          {text: '大(2)型', value: '1'},
-          {text: '中型', value: '2'},
-          {text: '小(1)型', value: '3'},
-          {text: '小(2)型', value: '4'}
-        ],
-        filterMultiple: true,
-        filteredValue: filteredInfo.userProjCount || null,
-        onFilter: (value, record) => record.userProjCount.includes(value)
-      }, {
-        title: '水库类型',
-        dataIndex: 'userFollowCount'
+        ]
+      }
+    },
+    mounted() {},
+    methods: {
+      // 添加水库
+      goAdd(){
+        this.$router.push("/system/reservoir/reservoir/reservoir_add")
       },
-      {
-        title: '创建时间',
-        dataIndex: 'lastLoginTime'
-      }, {
-        title: '操作',
-        dataIndex: 'operation',
-        customRender: (text, record) => (
-          <div class="icons-list">
-            <a-icon type="warning" theme="twoTone" twoToneColor="#4a9ff5" onClick={() => { this.edit(record) }} title="监测场景"></a-icon>
-            <a-icon type="info-circle" theme="twoTone" twoToneColor="#4a9ff5" onClick={() => { this.userDelete(record) }} title="监测点"></a-icon>
-            <a-icon type="user" onClick={() => { this.userDelete(record) }} title="用户"></a-icon>
-            <a-icon type="setting" theme="twoTone" twoToneColor="#4a9ff5" onClick={() => { this.edit(record) }} title="编辑"></a-icon>
-            <a-icon type="delete" theme="twoTone" twoToneColor="#4a9ff5" onClick={() => { this.userDelete(record) }} title="删除"></a-icon>
-          </div>
-        )
-      }]
-    }
-  },
-  mounted () {
-    this.fetch()
-  },
-  methods: {
-    add () {
-      this.userAdd.visiable = true
-    },
-    handleUserAddClose () {
-      this.userAdd.visiable = false
-    },
-    handleUserAddSuccess () {
-      this.userAdd.visiable = false
-      this.$message.success('新增用户成功，初始密码为1234qwer')
-      this.search()
-    },
-    edit (record) {
-      this.$refs.userEdit.setFormValues(record)
-      this.userEdit.visiable = true
-    },
-    handleUserEditClose () {
-      this.userEdit.visiable = false
-    },
-    handleUserEditSuccess () {
-      this.userEdit.visiable = false
-      this.$message.success('修改用户成功')
-      this.search()
-    },
-    handleUserInfoClose () {
-      this.userInfo.visiable = false
-    },
-    userDelete (record) {
-      let that = this
-      this.$confirm({
-        title: '确定删除该用户?',
-        content: '当您点击确定按钮后，这个用户将会被彻底删除',
-        centered: true,
-        onOk () {
-          let userIds = []
-          userIds.push(record.userId)
-          that.$post('server/user.php', {
-            userIds: userIds.join(','),
-            action: 'deleteUsers'
-          }).then(() => {
-            that.$message.success('删除成功')
+      userDelete(record) {
+        let that = this
+        this.$confirm({
+          title: '确定删除该用户?',
+          content: '当您点击确定按钮后，这个用户将会被彻底删除',
+          centered: true,
+          onOk() {
+            let userIds = []
+            userIds.push(record.userId)
+            that.$post('server/user.php', {
+              userIds: userIds.join(','),
+              action: 'deleteUsers'
+            }).then(() => {
+              that.$message.success('删除成功')
+              that.selectedRowKeys = []
+              that.search()
+            })
+          },
+          onCancel() {
             that.selectedRowKeys = []
-            that.search()
-          })
-        },
-        onCancel () {
-          that.selectedRowKeys = []
-        }
-      })
-    },
-    search () {
-      let {filteredInfo} = this
-      this.fetch({
-        ...this.queryParams,
-        ...filteredInfo
-      })
-    },
-    reset () {
-      // 重置分页
-      this.$refs.TableInfo.pagination.current = this.pagination.defaultCurrent
-      if (this.paginationInfo) {
-        this.paginationInfo.current = this.pagination.defaultCurrent
-        this.paginationInfo.pageSize = this.pagination.defaultPageSize
-      }
-      // 重置列过滤器规则
-      this.filteredInfo = null
-      // 重置查询参数
-      this.queryParams = {}
-      this.fetch()
-    },
-    handleTableChange (pagination, filters) {
-      // 将这三个参数赋值给Vue data，用于后续使用
-      this.paginationInfo = pagination
-      this.filteredInfo = filters
-      this.fetch({
-        ...this.queryParams,
-        ...filters
-      })
-    },
-    fetch (params = {}) {
-      // 显示loading
-      this.loading = true
-      if (this.paginationInfo) {
-        // 如果分页信息不为空，则设置表格当前第几页，每页条数，并设置查询分页参数
-        this.$refs.TableInfo.pagination.current = this.paginationInfo.current
-        this.$refs.TableInfo.pagination.pageSize = this.paginationInfo.pageSize
-        params.pageSize = this.paginationInfo.pageSize
-        params.pageNum = this.paginationInfo.current
-      } else {
-        // 如果分页信息为空，则设置为默认值
-        params.pageSize = this.pagination.defaultPageSize
-        params.pageNum = this.pagination.defaultCurrent
-      }
-      if (this.currentUser.type === '1') {
-        params.type = '1' // 项目管理员
-      }
-      this.$get('server/user.php', {
-        action: 'finduserpage',
-        ...params
-      }).then((r) => {
-        let data = r.data
-        const pagination = { ...this.pagination }
-        pagination.total = data.total
-        this.dataSource = data.records
-        this.pagination = pagination
-        // 数据加载完毕，关闭loading
-        this.loading = false
-      })
+          }
+        })
+      },
     }
   }
-}
+
 </script>
 <style lang="less" scoped>
   @import "../../../../static/less/Common";
+
   .icons-list /deep/ .anticon {
     margin-right: 10px;
     font-size: 18px;
   }
+
 </style>
