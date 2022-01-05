21be432f6a304d064476ca88248142e5a29ad3dd
wjj900212 <1368358776@qq.com>
2022-01-05 10:41:27 +0800

    修改水库管理列表icon 大小

--- src/views/system/reservoir/reservoir.vue
+++ src/views/system/reservoir/reservoir.vue
@@ -46,167 +46,167 @@
 </template>
 
 <script>
-  import {
-    mapState
-  } from 'vuex'
-  export default {
-    name: 'reservoir',
-    components: {},
-    data() {
-      return {
-        queryParams: {
-          name: '',
-          area: ''
+import {
+  mapState
+} from 'vuex'
+export default {
+  name: 'reservoir',
+  components: {},
+  data () {
+    return {
+      queryParams: {
+        name: '',
+        area: ''
+      },
+      loading: false,
+      dataSource: [{name: '121', roleId: 1}],
+      pagination: {
+        pageSizeOptions: ['10', '20', '30', '40', '100'],
+        defaultCurrent: 1,
+        defaultPageSize: 10,
+        showQuickJumper: true,
+        showSizeChanger: true,
+        showTotal: (total, range) => `显示 ${range[0]} ~ ${range[1]} 条记录，共 ${total} 条记录`
+      }
+    }
+  },
+  computed: {
+    ...mapState({
+      currentUser: state => state.account.user
+    }),
+    columns () {
+      let {
+        filteredInfo
+      } = this
+      filteredInfo = filteredInfo || {}
+      return [{
+        title: '序号',
+        customRender: (text, record, index) => `${index + 1}`
+      }, {
+        title: '水库名称',
+        dataIndex: 'name'
+      }, {
+        title: '地址',
+        dataIndex: 'roleName3'
+      }, {
+        title: '管理单位',
+        dataIndex: 'roleName2'
+      }, {
+        title: '安全状态',
+        dataIndex: 'roleId',
+        customRender: (text) => {
+          switch (text) {
+            case '0':
+              return '无风险'
+            case '1':
+              return '有风险'
+          }
         },
-        loading: false,
-        dataSource: [{name: '121',roleId:1}],
-        pagination: {
-          pageSizeOptions: ['10', '20', '30', '40', '100'],
-          defaultCurrent: 1,
-          defaultPageSize: 10,
-          showQuickJumper: true,
-          showSizeChanger: true,
-          showTotal: (total, range) => `显示 ${range[0]} ~ ${range[1]} 条记录，共 ${total} 条记录`
+        filters: [{
+          text: '无风险',
+          value: '0'
+        },
+        {
+          text: '有风险',
+          value: '1'
+        }
+        ],
+        filterMultiple: true,
+        filteredValue: filteredInfo.roleId || null,
+        onFilter: (value, record) => record.roleId.includes(value)
+      }, {
+        title: '监测场景',
+        dataIndex: 'roleName1'
+      },
+      {
+        title: '水库规模',
+        dataIndex: 'userProjCount',
+        customRender: (text) => {
+          switch (text) {
+            case '0':
+              return '大(1)型'
+            case '1':
+              return '大(2)型'
+            case '2':
+              return '中型'
+            case '3':
+              return '小(1)型'
+            case '4':
+              return '小(2)型'
+          }
+        },
+        filters: [{
+          text: '大(1)型',
+          value: '0'
+        },
+        {
+          text: '大(2)型',
+          value: '1'
+        },
+        {
+          text: '中型',
+          value: '2'
+        },
+        {
+          text: '小(1)型',
+          value: '3'
+        },
+        {
+          text: '小(2)型',
+          value: '4'
+        }
+        ],
+        filterMultiple: true,
+        filteredValue: filteredInfo.userProjCount || null,
+        onFilter: (value, record) => record.userProjCount.includes(value)
+      }, {
+        title: '水库类型',
+        dataIndex: 'userFollowCount'
+      },
+      {
+        title: '创建时间',
+        dataIndex: 'lastLoginTime'
+      }, {
+        title: '操作',
+        dataIndex: 'operation',
+        scopedSlots: {
+          customRender: 'operation'
         }
       }
+      ]
+    }
+  },
+  mounted () {},
+  methods: {
+    // 添加水库
+    goAdd () {
+      this.$router.push('/system/reservoir/reservoir/reservoir_add')
     },
-    computed: {
-      ...mapState({
-        currentUser: state => state.account.user
-      }),
-      columns() {
-        let {
-          filteredInfo
-        } = this
-        filteredInfo = filteredInfo || {}
-        return [{
-            title: '序号',
-            customRender: (text, record, index) => `${index + 1}`
-          }, {
-            title: '水库名称',
-            dataIndex: 'name'
-          }, {
-            title: '地址',
-            dataIndex: 'roleName3'
-          }, {
-            title: '管理单位',
-            dataIndex: 'roleName2'
-          }, {
-            title: '安全状态',
-            dataIndex: 'roleId',
-            customRender: (text) => {
-              switch (text) {
-                case '0':
-                  return '无风险'
-                case '1':
-                  return '有风险'
-              }
-            },
-            filters: [{
-                text: '无风险',
-                value: '0'
-              },
-              {
-                text: '有风险',
-                value: '1'
-              }
-            ],
-            filterMultiple: true,
-            filteredValue: filteredInfo.roleId || null,
-            onFilter: (value, record) => record.roleId.includes(value)
-          }, {
-            title: '监测场景',
-            dataIndex: 'roleName1'
-          },
-          {
-            title: '水库规模',
-            dataIndex: 'userProjCount',
-            customRender: (text) => {
-              switch (text) {
-                case '0':
-                  return '大(1)型'
-                case '1':
-                  return '大(2)型'
-                case '2':
-                  return '中型'
-                case '3':
-                  return '小(1)型'
-                case '4':
-                  return '小(2)型'
-              }
-            },
-            filters: [{
-                text: '大(1)型',
-                value: '0'
-              },
-              {
-                text: '大(2)型',
-                value: '1'
-              },
-              {
-                text: '中型',
-                value: '2'
-              },
-              {
-                text: '小(1)型',
-                value: '3'
-              },
-              {
-                text: '小(2)型',
-                value: '4'
-              }
-            ],
-            filterMultiple: true,
-            filteredValue: filteredInfo.userProjCount || null,
-            onFilter: (value, record) => record.userProjCount.includes(value)
-          }, {
-            title: '水库类型',
-            dataIndex: 'userFollowCount'
-          },
-          {
-            title: '创建时间',
-            dataIndex: 'lastLoginTime'
-          }, {
-            title: '操作',
-            dataIndex: 'operation',
-            scopedSlots: {
-              customRender: 'operation'
-            }
-          }
-        ]
-      }
-    },
-    mounted() {},
-    methods: {
-      // 添加水库
-      goAdd(){
-        this.$router.push("/system/reservoir/reservoir/reservoir_add")
-      },
-      userDelete(record) {
-        let that = this
-        this.$confirm({
-          title: '确定删除该用户?',
-          content: '当您点击确定按钮后，这个用户将会被彻底删除',
-          centered: true,
-          onOk() {
-            let userIds = []
-            userIds.push(record.userId)
-            that.$post('server/user.php', {
-              userIds: userIds.join(','),
-              action: 'deleteUsers'
-            }).then(() => {
-              that.$message.success('删除成功')
-              that.selectedRowKeys = []
-              that.search()
-            })
-          },
-          onCancel() {
+    userDelete (record) {
+      let that = this
+      this.$confirm({
+        title: '确定删除该用户?',
+        content: '当您点击确定按钮后，这个用户将会被彻底删除',
+        centered: true,
+        onOk () {
+          let userIds = []
+          userIds.push(record.userId)
+          that.$post('server/user.php', {
+            userIds: userIds.join(','),
+            action: 'deleteUsers'
+          }).then(() => {
+            that.$message.success('删除成功')
             that.selectedRowKeys = []
-          }
-        })
-      },
+            that.search()
+          })
+        },
+        onCancel () {
+          that.selectedRowKeys = []
+        }
+      })
     }
   }
+}
 
 </script>
 <style lang="less" scoped>
